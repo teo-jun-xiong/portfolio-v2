4b8866ba76a26cedb96c546e8912b9f97a0f3090
teo-jun-xiong <teojunxiong97@gmail.com>
2021-06-04 01:40:10 +0800

    Add animations

---
+++ js/list-items-selector.js
@@ -0,0 +1,58 @@
+function selectProject(index) {
+  var itemNames = document.querySelectorAll(".projects-content-left-content .item-name");
+  var itemDetails = document.querySelectorAll(".projects-content-right-content .item-detail");
+  var itemImages = document.querySelectorAll(
+    ".projects-content.section-content .projects-content-img"
+  );
+  for (var i = 0; i < itemNames.length; i++) {
+    if (index === i) {
+      itemNames[i].classList.add("active");
+
+      itemDetails[i].classList.remove("hide");
+
+      itemImages[i].classList.add("show");
+      itemImages[i].classList.remove("hide");
+    } else {
+      itemNames[i].classList.remove("active");
+
+      itemDetails[i].classList.add("hide");
+
+      itemImages[i].classList.remove("show");
+      itemImages[i].classList.add("hide");
+    }
+  }
+}
+
+function selectExperience(index) {
+  var itemNames = document.querySelectorAll(".experience-content-right-content .item-name");
+  var itemDetails = document.querySelectorAll(".experience-content-left-content .item-detail");
+
+  for (var i = 0; i < itemNames.length; i++) {
+    if (index === i) {
+      itemNames[i].classList.add("active");
+
+      itemDetails[i].classList.remove("hide");
+    } else {
+      itemNames[i].classList.remove("active");
+
+      itemDetails[i].classList.add("hide");
+    }
+  }
+}
+
+function selectSkills(index) {
+  var itemNames = document.querySelectorAll(".skills-content-right-content .item-name");
+  var itemDetails = document.querySelectorAll(".skills-content-left-content .item-detail");
+
+  for (var i = 0; i < itemNames.length; i++) {
+    if (index === i) {
+      itemNames[i].classList.add("active");
+
+      itemDetails[i].classList.remove("hide");
+    } else {
+      itemNames[i].classList.remove("active");
+
+      itemDetails[i].classList.add("hide");
+    }
+  }
+}
